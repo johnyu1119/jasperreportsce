67ac9435060f4d144cb785adfe8983bdb4155a15
johnyu1119 <johnyu1119@users.noreply.github.com>
2021-07-07 16:13:18 +0800

    Update app-entrypoint.sh

--- 7/debian-10/rootfs/app-entrypoint.sh
+++ 7/debian-10/rootfs/app-entrypoint.sh
@@ -9,5 +9,5 @@ if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
     nami_initialize tomcat mysql-client jasperreports
     info "Starting gosu2... "
 fi
-info "exect tini..."
+info "exect tini1..."
 # exec tini -- "$@"
