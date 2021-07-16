5c8440c4f49388da1d21d95b1da07c6d4edce79a
johnyu1119 <johnyu1119@users.noreply.github.com>
2021-07-16 17:04:04 +0800

    Update bitnami-user.sh

--- 7/debian-10/prebuildfs/build/bitnami-user.sh
+++ 7/debian-10/prebuildfs/build/bitnami-user.sh
@@ -4,5 +4,5 @@ useradd -ms /bin/bash bitnami
 mkdir -p /opt/bitnami
 sed -i -e 's/\s*Defaults\s*secure_path\s*=/# Defaults secure_path=/' /etc/sudoers
 echo 'bitnami ALL=NOPASSWD: ALL' >> /etc/sudoers
-echo '%root ALL=(ALL:ALL) ALL' >> /etc/sudoers
-echo '%root ALL=NOPASSWD: ALL' >> /etc/sudoers
+# echo '%root ALL=(ALL:ALL) ALL' >> /etc/sudoers
+# echo '%root ALL=NOPASSWD: ALL' >> /etc/sudoers
